11638e4aa1fc1ef10c78e66cc59ecd7f980c0843
Kulmamatov Zalkar <kulmamatov.zalkar@gmail.com>
2022-05-18 19:12:10 +0600

    Lesson 4

---
+++ src/components/HelloComponent.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+// export default function Hello(props) {
+//   let { name } = props;
+
+//   return <h1>Hello, {name}</h1>;
+// }
+
+// export default class Hello extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, {this.props.name}</h1>
+//         <h1>Hello, {this.props.surname}</h1>
+//       </div>
+//     );
+//   }
+// }
+// export default Hello;
+
+// class Hello1 extends Component {
+//   render() {
+//     return <h1>Hello, Class Component</h1>;
+//   }
+// }
+
+// let Hello3 = () => {
+//   return <h1>Hello, Arrow Func Component</h1>;
+// };
+
+// export { Hello, Hello1, Hello3 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+// import { Hello, Hello1, Hello3 } from "./components/HelloComponent.js";
+import Hello from "./components/HelloComponent";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootNode = document.getElementById("root");
+const root = ReactDOM.createRoot(rootNode);
+
+const user = {
+  id: 5,
+  age: 33,
+  firstName: "Faha",
+  lastName: "Muhidinov",
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+const userStyle = {
+  fontSize: "22px",
+  backgroundColor: "#ccc",
+  padding: "10px",
+};
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Hello name="Eugene" surname="Sagaz" />
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+будут 2 компонента app и menu-item реализовать меню с помощью передачи данных через props (атрибут компонента) и map
+
+// root.render(
+//   <div id={user.id} className="user" style={userStyle}>
+//     <p>FullName: {user.getFullName()}</p>
+//     <p>Age: {user.age}</p>
+//     <p>Created At: {new Date().toLocaleTimeString()}</p>
+//   </div>
+// );
+
+// const element = <h1>Hello, React</h1>;
+
+// root.render(
+//   <div id="items">
+//     <h2>Список Телефонов</h2>
+//     <ul>
+//       <li>Apple Iphone 12</li>
+//       <li>Samsung Galaxy s22</li>
+//       <li>Huaweu P50</li>
+//       <li>MI 11 ultra</li>
+//     </ul>
+//   </div>
+// );
+
+// function tick() {
+//   root.render(<h2>Текущее время {new Date().toLocaleTimeString()}</h2>);
+// }
+// setInterval(tick, 1000);
+
+// function Hello() {
+//   return <h1>Hello, Eugene</h1>;
+// }
+
+// const Hello2 = () => {
+//   return <h2>Hello, Timoti</h2>;
+// };
+
+// class Hello3 extends Component {
+//   render() {
+//     return <h3>Hello, Faha</h3>;
+//   }
+// }
+
+// root.render(
+//   <div>
+//     <Hello />
+//     <Hello />
+//     <Hello />
+//     <Hello />
+//     <Hello2 />
+//     <Hello3 />
+//   </div>
+// );
